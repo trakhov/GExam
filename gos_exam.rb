b9e6e78553ad104c7b05a37987380bce5f906efe
Igor Trakhov <igor.trakhov@gmail.com>
2014-06-03 06:41:02 +0400

    unified examCards

--- gos_exam.rb
+++ gos_exam.rb
@@ -1,15 +1,21 @@
 [
-# 	{spec: '080507.65', type: 'Менеджмент в таможенном деле', group: 'm91_tamozh', sort: 'straight', title: 'Менеджмент организации'},
-# 	{spec: '080507.65', type: 'Гостиничный и туристический бизнес', group: 'm91_hotel', sort: 'straight', title: 'Менеджмент организации'},
-# 	{spec: '080507.65', type: 'Управление в страховании', group: 'm91_insurance', sort: 'straight', title: 'Менеджмент организации'},
-# 	{spec: '080507.65', type: 'Менеджмент в массовом питании', group: 'm91_food', sort: 'straight', title: 'Менеджмент организации'},
-# 	{spec: '080507.65', type: 'Гостиничный и туристический бизнес', group: 'm91_hotel', sort: 'straight', title: 'Менеджмент организации'},
-# 	{spec: '080507.65', type: 'Менеджмент торговых предприятий', group: 'm91_trade', sort: 'straight', title: 'Менеджмент организации'},
 
-# 	{spec: '080109.65', title: 'Бухгалтерский учет, анализ и аудит', sort: 'straight', group: 'b91'},
+# Специалитет:
 
-	{spec: '100101.65', title: 'Cервис', sort: 'sorted', group: 'a91'},
+	# {spec: '080507.65', type: 'Менеджмент в таможенном деле', group: 'm91_tamozh', sort: 'straight', title: 'Менеджмент организации'},
+	# {spec: '080507.65', type: 'Гостиничный и туристический бизнес', group: 'm91_hotel', sort: 'straight', title: 'Менеджмент организации'},
+	# {spec: '080507.65', type: 'Управление в страховании', group: 'm91_insurance', sort: 'straight', title: 'Менеджмент организации'},
+	# {spec: '080507.65', type: 'Менеджмент в массовом питании', group: 'm91_food', sort: 'straight', title: 'Менеджмент организации'},
+	# {spec: '080507.65', type: 'Гостиничный и туристический бизнес', group: 'm91_hotel', sort: 'straight', title: 'Менеджмент организации'},
+	# {spec: '080507.65', type: 'Менеджмент торговых предприятий', group: 'm91_trade', sort: 'straight', title: 'Менеджмент организации'},
+
+	# {spec: '080109.65', title: 'Бухгалтерский учет, анализ и аудит', sort: 'straight', group: 'b91'},
+
+	# {spec: '100101.65', title: 'Cервис', sort: 'sorted', group: 'a91'},
 	# {spec: '100103.65', title: 'Социально-культурный сервис и туризм', sort: 'sorted', group: 't91'},
+
+
+# Бакалавриат:
 
 	# {spec: '100100.62', title: 'Сервис', sort: 'sorted', group: 's10'},
 	# {spec: '080100.62', title: 'Экономика', sort: 'sorted', group: 'e10'},
