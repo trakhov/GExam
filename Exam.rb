3d11b127d66359c0ba58b17a452727920a4e6228
Igor Trakhov <igor.trakhov@gmail.com>
2014-06-08 00:45:11 +0400

    prepared first exam

--- Exam.rb
+++ Exam.rb
@@ -1,33 +1,59 @@
 
 
 class Exam
-	attr_reader :data
 
 	def initialize(data)
-		@d = data
+		data.each do |k, v|
+			instance_variable_set "@#{k}", v
+		end
+	end
+	
+
+	def add_tasks_from_file(dir)
+		# если две и более группы, берем первую
+		spec = @spec.split(', ')[0]
+		open(File.join dir, spec, @course + '.rb') do |file|
+			@tasks = eval file.read
+		end
 	end
 
 
-	def pr
-		@d.each do |k, v|
-			print "%8s: %s\n" % [k, v]
+	def make_cards(cmd)
+		cards = ''
+		@tasks.each do |card|
+			questions = card.join "\n\t;;\n\t"
+			cards += "\n#{cmd} {\n\t#{questions}\n}\n"
 		end
+		cards
+	end
+
+
+	def get_out_name(dir)
+		filename = "#{@spec} #{@course}.tex"
+		File.join 'output', dir, filename
+	end
+
+
+	def renew(opts)
+		# opts = @@defaults.merge options
+		opts.map do |k, v|
+			"\\renewcommand{\\#{k.to_s}}{#{v}}"
+		end.join("\n")
 	end
 
 
 	class << self
-		def collect
+		
+		def collect(list)
 			out = []
-
-			open(self.list) { |f| eval f.read }.each do |hash|
+			open(list) { |f| eval f.read }.each do |hash|
 				exam = self.new hash
 				exam.add_tasks_from_file
-
 				out << exam
 			end
-			
 			out
 		end
+
 	end
 
 end
@@ -35,25 +61,73 @@ end
 
 
 class SemEx < Exam
-	@@list = 'ExamList.rb'
-	@@dir = 'txt'
+	@@list = 		'ExamList.rb'
+	@@dir = 		'txt'
+	@@main = 		open('tmpl/SE_main.tex') { |f| f.read }
+	@@cmd = 		'\ExamCard'
+	@@out_dir =	'Semester' 
 
-	def self.list
-		@@list
-	end
+
+
+	def prepare(opts: {}, subl: true)
+		defaults = {
+			spec: @spec,
+			disc: @course,
+			kaf: @kaf
+		}
+		options = defaults.merge opts
+		cards = make_cards @@cmd
+		out = get_out_name @@out_dir
+
+		open(out, 'w') do |file|
+			file.write @@main % {
+				renews: renew(options),
+				teacher: @teacher,
+				zav: @zav,
+				cards: cards
+			}
+		end
+
+		`subl '#{out}'` if subl
+	end	
+
 
 	def add_tasks_from_file
-		open(File.join @@dir, @d[:spec], @d[:course] + '.rb') do |file|
-			@d[:tasks] = eval file.read
-		end
+		super @@dir 
 	end
+
+
+	class << self
+
+		def collect
+			super @@list
+		end
+
+	end
+
 
 end
 
 
+# todo:
+# class GExam < Exam	
+# 	@@list = 		'GExamList.rb'
+# 	@@dir = 		'txt/GE'
+# 	@@main = 		open('tmpl/GE_main.tex') { |f| f.read }
+# 	@@cmd = 		'\GExamCard'
+# 	@@out_dir =	'Госэкзамены' 
+
+
+# 	def initialize(data)
+# 		super 
+# 		@course = 'ГОСЭКЗАМЕН'
+# 	end
+
+	
+# end
 
 
 
-sem = SemEx.collect
+e = SemEx.collect[0]
 
-p sem[0]
+e.prepare opts: {protocol: '13.12.2014'}
--- misc.rb
+++ misc.rb
@@ -1,4 +1,2 @@
-# -*- coding: utf-8
-
-# p Dir.pwd
-open(Dir.glob("txt/101101/Менеджмент.txt")[0], encoding: 'utf-8') { |f| print f.read }
+# p Dir.glob("output/Cеместровые/*")
+p Dir.glob("output/*")
